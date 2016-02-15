f05c4a35d3fea12ad1cf29b3f06e3291d17a74a2
Mason O'Mara <mason.omara@shoutlet.com>
2016-02-15 08:18:13 -0600

    adding in dev flag

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ var path = require('path');
 var express = require('express');
 var app = new (require('express'))();
 
-var config = require('./webpack.config');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var compiler = webpack(config);
-
 var port = process.env.PORT || 3000;
 
 var isDevelopment = (process.env.NODE_ENV !== 'production');
 
 var static_path = path.join(__dirname, 'public');
 
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
-
+if (isDevelopment) {
+    var webpackDevMiddleware = require('webpack-dev-middleware');
+    var webpackHotMiddleware = require('webpack-hot-middleware');
+    var config = require('./webpack.config');
+    var compiler = webpack(config);
+    app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+    app.use(webpackHotMiddleware(compiler));
+}
 
 app.use(express.static(static_path))
     .get("/", function(req, res) {
