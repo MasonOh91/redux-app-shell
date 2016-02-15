789e34a081cc9da5b932e918309442552ee7c27a
Mason O'Mara <mason.omara@shoutlet.com>
2016-02-14 22:12:13 -0600

    moving dev dependencies, adding dev conditions on express server

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
-
+var path = require('path');
+var express = require('express');
 var app = new (require('express'))();
-var port = 3000;
 
-var compiler = webpack(config);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
+var port = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+
+var static_path = path.join(__dirname, 'public');
+
+if (isDevelopment) {
+    var webpackDevMiddleware = require('webpack-dev-middleware');
+    var webpackHotMiddleware = require('webpack-hot-middleware');
+
+    var compiler = webpack(config);
+    app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+    app.use(webpackHotMiddleware(compiler));
+}
+
+app.use(express.static(static_path))
+    .get("/", function(req, res) {
+      res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
-  }
+    app.listen(port, function(error) {
+      if (error) {
+        console.error(error);
+    } else {
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+        console.log("==> 🌎  Environment is currently: " + (isDevelopment === true ? "Development" : "Production"));
+    }
 });
