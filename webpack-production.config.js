0ea97d767274d66745f5961561e0bfca70125113
Mason O'Mara <mason.omara@shoutlet.com>
2016-02-14 23:01:36 -0600

    changing some configs for pushing to prod

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,11 +1,9 @@
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     devtool: 'source-map',
     entry: [
-        'webpack-hot-middleware/client',
         './index'
     ],
     output: {
@@ -21,10 +19,6 @@ module.exports = {
                 exclude: /node_modules/,
                 include: __dirname
             },
-            // --- The following lines will break out our compiled css file into a separte file, which could be nice for prod, breaks --
-            // --- hot reloading stlyes however --
-            // { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap") },
-            // { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap!sass-loader?sourceMap") },
             { test: /\.css$/, loaders: ["style-loader", "css-loader?sourceMap"] },
             { test: /\.scss$/, loaders: ["style-loader", "css-loader?sourceMap!sass-loader?sourceMap"] },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
@@ -32,12 +26,5 @@ module.exports = {
             { test: /\.(jpg|jpeg|gif|png|ico)$/, exclude: /node_modules/, loader: 'url-loader?limit=8192' },
             { test: /\.(ico)$/, loader: "static-loader" }
         ]
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-        // -- breaks out css
-        // new ExtractTextPlugin('layout.css')
-    ]
+    }
 }
